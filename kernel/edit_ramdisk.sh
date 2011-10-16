b602c1f88582e39f672fc9a9e6d2a3dcebb4a7c6
Tassadar <vbocek@gmail.com>
2011-10-16 17:55:55 +0200

    Make it working

--- kernel/edit_ramdisk.sh
+++ kernel/edit_ramdisk.sh
@@ -1,9 +1,12 @@
 #!/sbin/sh
-mkdir -p /tmp/rd
+rm -r /tmp/rd
+mkdir /tmp/rd
 cd /tmp/rd
-/tmp/busybox_arch gzip -d -c /tmp/boot.img-ramdisk.gz | /tmp/busybox_arch cpio -i
+/tmp/busybox_arch gzip -d -c /tmp/boot.img-ramdisk.gz | /tmp/busybox_arch cpio -id
 mv /tmp/rd/init /tmp/rd/main_init
 cp -r -p /tmp/ramdisk/* /tmp/rd/
-ls | /tmp/busybox_arch cpio -o -Hnewc > ../new_rd.cpio
-/tmp/busybox_arch gzip /tmp/new_rd.cpio
+rm /tmp/new_rd.cpio
+rm /tmp/new_rd.cpio.gz
+chmod 750 /tmp/rd/init
+find . | /tmp/busybox_arch cpio -o -H newc | /tmp/busybox_arch gzip > ../new_rd.cpio.gz
 return 0
