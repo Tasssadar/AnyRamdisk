2ea5f3c3988514e4b69c513dd69621c4befba91d
Tassadar <vbocek@gmail.com>
2011-12-10 13:17:29 +0100

    Create uninstaller

--- kernel/edit_ramdisk.sh
+++ kernel/edit_ramdisk.sh
@@ -3,33 +3,26 @@ rm -r /tmp/rd
 mkdir /tmp/rd
 cd /tmp/rd
 /tmp/busybox_arch gzip -d -c /tmp/boot.img-ramdisk.gz | /tmp/busybox_arch cpio -i
+
+#exit if no main_init found
 if [ ! -f /tmp/rd/main_init ];
 then
-    mv /tmp/rd/init /tmp/rd/main_init
+    exit 1
 fi
 
-# Remove old version images
+# Restore init
+rm /tmp/rd/init
+mv /tmp/rd/main_init /tmp/rd/init
+rm /tmp/rd/preinit.rc
+
+# Remove images
 rm /tmp/rd/init_0.rle
 rm /tmp/rd/init_1.rle
-rm /tmp/rd/bmgr_imgs/init_0_sel.rle
-rm /tmp/rd/bmgr_imgs/init_1_sel.rle
-rm /tmp/rd/bmgr_imgs/init_rbt.rle
+rm -r /tmp/rd/bmgr_imgs
 
-cp -r -p /tmp/ramdisk/* /tmp/rd/
+# Pack ramdisk again
 rm /tmp/new_rd.cpio
 rm /tmp/new_rd.cpio.gz
 chmod 750 /tmp/rd/init
 find . | /tmp/busybox_arch cpio -o -H newc | /tmp/busybox_arch gzip > ../new_rd.cpio.gz
-busybox mount -t auto /dev/block/mmcblk0p2 /sd-ext
-mkdir -p /sd-ext/multirom
-busybox umount /sd-ext
-
-# Create multirom config file
-if [ ! -f /sdcard/multirom.txt ];
-then
-    echo "timezone = 0" > /sdcard/multirom.txt
-    echo "timeout = 3" >> /sdcard/multirom.txt
-    echo "show_seconds = 0" >> /sdcard/multirom.txt
-fi
-
 return 0
